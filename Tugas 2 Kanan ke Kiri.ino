5b9082337738fd9eb9f94b8d50e977640dc4b3f7
Ririhan <alrihan301@gmail.com>
2024-09-21 22:05:18 +0700

    Tugas 2

---
+++ Tugas 2 Kanan ke Kiri.ino
@@ -0,0 +1,22 @@
+void setup() {
+  for(int LED = 2; LED<=4; LED++){
+    pinMode(LED, OUTPUT);
+  }
+}
+void loop() {
+
+for(int LED = 4; LED<=2; LED++){
+  digitalWrite(LED,LOW);
+  delay(250);
+}
+
+for(int LED =4; LED>=2; LED--){
+  digitalWrite(LED,HIGH);
+  delay(250);
+  digitalWrite(LED,LOW);
+}
+for(int LED = 2; LED<=4; LED++){
+  digitalWrite(LED,LOW);
+  delay(250);
+}
+}
